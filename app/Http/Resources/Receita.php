31f02145cfc4325b51fd7ee23a424417a2fdb076
Styl Bandeira de Amorim Coelho <33157055+stylbandeira@users.noreply.github.com>
2022-03-09 15:48:08 -0300

    Definição de Campos - Resource Receita

--- app/Http/Resources/Receita.php
+++ app/Http/Resources/Receita.php
@@ -14,6 +14,13 @@ class Receita extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id'        =>  $this.id,
+            'nome'      =>  $this.nome,
+            'descricao' =>  $this.descricao,
+            'nivel'     =>  $this.nivel,
+            'qualidade' =>  $this.qualidade
+        ];
     }
 }
