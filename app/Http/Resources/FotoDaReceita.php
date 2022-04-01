6b28a3e817631c69004e77ea0453220847bcf608
Styl Bandeira de Amorim Coelho <33157055+stylbandeira@users.noreply.github.com>
2022-04-01 10:28:53 -0300

    Sem comentários - MFotosDaReceita
    Tirei comentários do Model FotosDaReceita

--- app/Http/Resources/FotoDaReceita.php
+++ app/Http/Resources/FotoDaReceita.php
@@ -14,7 +14,6 @@ class FotoDaReceita extends JsonResource
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
         return [
             'id' => $this->id,
             'url_img' => $this->url_img,
