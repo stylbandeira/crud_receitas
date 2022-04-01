ef445915599864fc5f921db3deb389202eba1c35
Styl Bandeira de Amorim Coelho <33157055+stylbandeira@users.noreply.github.com>
2022-04-01 10:25:09 -0300

    Tirei comentários
    Tirei comentários da rota e comentei umas coisas que eu não lembrava

--- routes/api.php
+++ routes/api.php
@@ -22,97 +22,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/**
- * ROTAS DAS RECEITAS
- * 
- */
-
-//Lista de Receitas
 Route::get('receitas', [ReceitaController::class, 'index']);
-
-//Single Receita
 Route::get('receita/{id}', [ReceitaController::class, 'show']);
-
-//Cria uma nova receita
 Route::post('receitas', [ReceitaController::class, 'store']);
-
-//Atualiza uma receita
 Route::put('receita/{id}', [ReceitaController::class, 'update']);
-
-//Deleta uma receita
 Route::delete('receita/{id}', [ReceitaController::class, 'destroy']);
 
-/**
- * ROTAS DAS RECEITAS
- * FIM
- */
-
-//------------------------------------------------------------------------------------
-
-/**
- * ROTAS DAS CATEGORIAS
- * 
- */
-
-//Lista de Categorias
 Route::get('categorias', [CategoriaController::class, 'index']);
-
-//Single Categoria
 Route::get('categoria/{id}', [CategoriaController::class, 'show']);
-
-//Cria uma nova Categoria
 Route::post('categorias', [CategoriaController::class, 'store']);
-
-//Atualiza uma Categoria
 Route::put('categoria/{id}', [CategoriaController::class, 'update']);
-
-//Deleta uma Categoria
 Route::delete('categoria/{id}', [CategoriaController::class, 'destroy']);
 
- /**
- * ROTAS DAS CATEGORIAS
- * FIM
- */
-
-//------------------------------------------------------------------------------------
-
-//Lista de Categorias das Receitas
 Route::get('categoriasreceitas', [CategoriaReceitaController::class, 'index']);
-
-//Single Categoria
 Route::get('categoriareceita/{id_receita}', [CategoriaReceitaController::class, 'show']);
-
-//Cria uma nova Categoria
 Route::post('categoriasreceitas', [CategoriaReceitaController::class, 'store']);
-
-//Atualiza uma Categoria
 Route::put('categoriasreceita/{id_receita}', [CategoriaReceitaController::class, 'update']);
-
-//Deleta uma Categoria
 Route::delete('categoriasreceita/{id_receita}', [CategoriaController::class, 'destroy']);
-/**
- * ROTAS DAS CATEGORIAS DAS RECEITAS
- * FIM
- */
 
 
- //------------------------------------------------------------------------------------
-
-//Lista de Fotos das Receitas
-Route::get('categoriasreceitas', [FotoDaReceitaController::class, 'index']);
-
-//Single Foto
-Route::get('categoriareceita/{id}', [FotoDaReceitaController::class, 'show']);
-
-//Cria uma nova Foto
-Route::post('categoriasreceitas', [FotoDaReceitaController::class, 'store']);
-
-//Atualiza uma Foto
-Route::put('categoriasreceita/{id}', [FotoDaReceitaController::class, 'update']);
-
-//Deleta uma Foto
-Route::delete('categoriasreceita/{id}', [FotoDaReceitaController::class, 'destroy']);
-/**
- * ROTAS DAS FOTOS DAS RECEITAS
- * FIM
- */
+//Ok eu não to lembrando porque eu fiz essas coisas, então eu vou comentar...
+// Route::get('categoriasreceitas', [FotoDaReceitaController::class, 'index']);
+// Route::get('categoriareceita/{id}', [FotoDaReceitaController::class, 'show']);
+// Route::post('categoriasreceitas', [FotoDaReceitaController::class, 'store']);
+// Route::put('categoriasreceita/{id}', [FotoDaReceitaController::class, 'update']);
+// Route::delete('categoriasreceita/{id}', [FotoDaReceitaController::class, 'destroy']);
