8b54a9828c6bc842fd12e64d1511e94e6eaf4ca6
Styl Bandeira de Amorim Coelho <33157055+stylbandeira@users.noreply.github.com>
2022-04-01 10:28:14 -0300

    Sem comentários - MReceita
    Tirei comentários do Model de Receita

--- app/Http/Resources/Receita.php
+++ app/Http/Resources/Receita.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Receita extends JsonResource{
     public function toArray($request){
-        // return parent::toArray($request);
         return [
             'id'        =>  $this->id,
             'nome'      =>  $this->nome,
