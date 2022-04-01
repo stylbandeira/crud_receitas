36879d0c115a31524d62ee5e07c56adfcc8fde3e
Styl Bandeira de Amorim Coelho <33157055+stylbandeira@users.noreply.github.com>
2022-04-01 10:29:14 -0300

    Sem comentários - CategoriaDaReceita

--- app/Http/Resources/CategoriaReceita.php
+++ app/Http/Resources/CategoriaReceita.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class CategoriaReceita extends JsonResource{
     public function toArray($request)
     {
-        // return parent::toArray($request);
         return [
             'id' => $this->id,
             'id_categoria' => $this->id_categoria,
