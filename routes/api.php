e2a8e9ade40392b82b3ff5d156af058c148b6409
Styl Bandeira de Amorim Coelho <33157055+stylbandeira@users.noreply.github.com>
2022-04-14 15:24:45 -0300

    Route::group

--- routes/api.php
+++ routes/api.php
@@ -23,31 +23,29 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('receitas', [ReceitaController::class, 'index']);
-Route::get('receitas/{receita}', [ReceitaController::class, 'show']);
-Route::post('receitas', [ReceitaController::class, 'store']);
-Route::put('receitas/{receita}', [ReceitaController::class, 'update']);
-Route::delete('receitas/{id}', [ReceitaController::class, 'destroy']);
+Route::group(['prefix' => 'receitas'], function (){
+    Route::get('', [ReceitaController::class, 'index']);
+    Route::get('/{receita}', [ReceitaController::class, 'show']);
+    Route::post('', [ReceitaController::class, 'store']);
+    Route::put('/{receita}', [ReceitaController::class, 'update']);
+    Route::delete('/{id}', [ReceitaController::class, 'destroy']);
+});
 
-// Route::apiResource('receita', 'App\Http\Controllers\ReceitaController');
+Route::group(['prefix' => 'categorias'], function (){
+    Route::get('', [CategoriaController::class, 'index']);
+    Route::get('/{id}', [CategoriaController::class, 'show']);
+    Route::post('', [CategoriaController::class, 'store']);
+    Route::put('/{id}', [CategoriaController::class, 'update']);
+    Route::delete('/{id}', [CategoriaController::class, 'destroy']);
+});
 
-Route::get('categorias', [CategoriaController::class, 'index']);
-Route::get('categorias/{id}', [CategoriaController::class, 'show']);
-Route::post('categorias', [CategoriaController::class, 'store']);
-Route::put('categorias/{id}', [CategoriaController::class, 'update']);
-Route::delete('categorias/{id}', [CategoriaController::class, 'destroy']);
-
-Route::get('categoriasreceitas', [CategoriaReceitaController::class, 'index']);
-Route::get('categoriareceita/{id_receita}', [CategoriaReceitaController::class, 'show']);
-Route::post('categoriasreceitas', [CategoriaReceitaController::class, 'store']);
-Route::put('categoriasreceita/{id_receita}', [CategoriaReceitaController::class, 'update']);
-Route::delete('categoriasreceita/{id_receita}', [CategoriaController::class, 'destroy']);
-
-Route::get('imagens', [FotoDaReceitaController::class, 'index']);
-Route::get('imagens/{id}', [FotoDaReceitaController::class, 'show']);
-Route::post('imagens', [FotoDaReceitaController::class, 'store']);
-Route::put('imagens/{id}', [FotoDaReceitaController::class, 'update']);
-Route::delete('imagens/{id}', [FotoDaReceitaController::class, 'destroy']);
+Route::group(['prefix' => 'imagens'], function (){
+    Route::get('', [FotoDaReceitaController::class, 'index']);
+    Route::get('/{id}', [FotoDaReceitaController::class, 'show']);
+    Route::post('', [FotoDaReceitaController::class, 'store']);
+    Route::put('/{id}', [FotoDaReceitaController::class, 'update']);
+    Route::delete('/{id}', [FotoDaReceitaController::class, 'destroy']);
+});
 
 Route::group(['prefix' => 'etapas'], function (){
     Route::get('/{id}', [EtapaController::class, 'show']);
@@ -55,11 +53,3 @@ Route::group(['prefix' => 'etapas'], function (){
     Route::put('/{id}', [EtapaController::class, 'update']);
     Route::delete('/{id}', [EtapaController::class, 'destroy']);
 });
-
-
-//Ok eu não to lembrando porque eu fiz essas coisas, então eu vou comentar...
-// Route::get('categoriasreceitas', [FotoDaReceitaController::class, 'index']);
-// Route::get('categoriareceita/{id}', [FotoDaReceitaController::class, 'show']);
-// Route::post('categoriasreceitas', [FotoDaReceitaController::class, 'store']);
-// Route::put('categoriasreceita/{id}', [FotoDaReceitaController::class, 'update']);
-// Route::delete('categoriasreceita/{id}', [FotoDaReceitaController::class, 'destroy']);
